32168594b70134e079ff1573c9f8515b8ea54ac1
Steve Marshall <stevesonoa@gmail.com>
2018-06-24 22:33:16 -0700

    Added checks for Straight & Pair. Updated initialHand template.

--- src/utils/handChecker.js
+++ src/utils/handChecker.js
@@ -1,20 +1,22 @@
-initialHand = {
+const initialHand = {
     cards: [
         { value: 8, suit: 'clubs' },
         { value: 13, suit: 'hearts' },
         { value: 12, suit: 'hearts' },
-        { value: 11, suit: 'diamonds' },
-        { value: 6, suit: 'hearts' },
+        { value: 13, suit: 'diamonds' },
+        { value: 9, suit: 'hearts' },
         { value: 8, suit: 'hearts' },
         { value: 10, suit: 'hearts' }
-    ],
-	suit: null,
-	sort: 'none',
-	handTitle: 'High Card',
-	handRank: 1,
-	cardOrder: []
-	// To check win condition, the player with the highest handRank wins.
-	// Tiebreakers are with cardOrder[0].value, then c[1].value, then c[2].value, etc. If no winner after cardOrder.length, it's a tie.
+    ], // The player's 7 card pool to make a hand from, including 2 hole cards & 5 table cards
+	suit: null, // If at least 5 cards share the same suit, that suit will be marked here as a string value
+	sort: 'none', // 'none', 'valueSort', or 'suitSort' to reflect what order this.cards are currently in
+	handTitle: 'High Card', // String syntax for a hand's title
+	handRank: 1, // This is the card rank. 1 is a High Card; 10 is a Royal Flush.
+	cardOrder: [], // This is the resulting 5 card hand
+		// To check win condition, the player with the highest handRank wins.
+		// Tiebreakers are with cardOrder[0].value, then c[1].value, then c[2].value, etc. If no winner after cardOrder.length, it's a tie.
+	pairCards: [], // If there is a pair, the system will log a copy of cardOrder so it will not be overwritten by future checks.
+	numPairs: 0 // Stores the number of pairs found
 }
 
 const valueSort = (hand) => {
@@ -65,16 +67,16 @@ const suitSort = (hand) => {
 		}
 	}
 	
-    if(hearts.length > clubs.length && hearts.length > diamonds.length && hearts.length > spades.length) {
+    if(hearts.length > 4) {
         suit = 'hearts';
     }
-    else if(clubs.length > hearts.length && clubs.length > diamonds.length && clubs.length > spades.length) {
+    else if(clubs.length > 4) {
         suit = 'clubs';
     }
-    else if(diamonds.length > hearts.length && diamonds.length > clubs.length && diamonds.length > spades.length) {
+    else if(diamonds.length > 4) {
         suit = 'clubs';
     }
-    else if(spades.length > hearts.length && spades.length > clubs.length && spades.length > diamonds.length) {
+    else if(spades.length > 4) {
         suit = 'clubs';
 	}
 	
@@ -154,41 +156,127 @@ const checkForFlush = (hand) => {
 	return resultingHand;
 };
 
-// valueSort
-// suitSort
-// if i[0].suit === i[4].suit || i[1] === i[5] || i[2] === i[6]
-// FLUSH EXISTS
-
 // STRAIGHT
-// valueSort
-// count suitSort
-// case hearts === 5, suit = "hearts"
-// case diamonds === 5, suit = "diamonds"
-// case spades === 5, suit = "spades"
-// case clubs === 5, suit = "clubs"
-// default: suit = null
-// if i[0] === i[1]
-// 	if suit === i[0].suit
-// 		push i[0] to new array
-// 	else
-// 		push i[1] to new array
-// else
-// 	i++
-// if i[0].value - 4 === i[4].value, STRAIGHT EXISTS
-// else if i[1].value - 4 === i[5].value, STRAIGHT EXISTS
-// else if i[2].value - 4 === i[6].value, STRAIGHT EXISTS
+const checkForStraight = (hand) => {
+	let resultingHand = {
+		...hand
+	};
+
+	let sequential = 0;
+	let lastIndex = 0;
+
+	const cards = [];
+
+	const updateStraight = (currentHand, i) => {
+		currentHand.handTitle = 'Straight';
+		currentHand.handRank = 5;
+		currentHand.cardOrder = [cards[i-4], cards[i-3], cards[i-2], cards[i-1], cards[i]];
+		return currentHand;
+	};
+
+	// Sort by value; suit doesn't matter
+	if(resultingHand.sort !== 'valueSort') {
+		resultingHand = valueSort(resultingHand);
+	}
+
+	// Now that resultingHand is sorted by value, add to the cards array if it's not a duplicate value
+	for (let i = 0; i < resultingHand.cards.length; i++) {
+		if (cards.length < 5 &&
+		(
+			i + 1 === resultingHand.cards.length ||
+			(i + 1 < resultingHand.cards.length && resultingHand.cards[i].value !== resultingHand.cards[i+1].value)
+		)) {
+			cards.push(resultingHand.cards[i]);
+		}
+	}
+
+	// If the current card is sequential with the previous card, and we don't already have 5 cards in order, update sequential and the lastIndex.
+	for (let i = 1; i < cards.length; i++) {
+		if(sequential < 4 && cards[i].value === cards[i-1].value - 1) {
+			sequential++;
+			lastIndex = i;
+		}
+		else if (sequential < 4) {
+			sequential = 0;
+		}
+	}
+
+	if (sequential >= 4) {
+		resultingHand = updateStraight(resultingHand, lastIndex);
+		resultingHand.cardsOrder = [cards[0], cards[1], cards[2], cards[3], cards[4]];
+		resultingHand.straight = true;
+	}
+
+	return resultingHand;
+};
 
 // 3 OF A KIND
-// check for PAIR
-// if # of pairs === 3 & value[0] === value[1], 3 OF A KIND EXISTS
+const checkForThreeKind = (hand) => {
+	// check for PAIR
+	// if # of pairs === 3 & value[0] === value[1], 3 OF A KIND EXISTS
+};
 
 // 2 PAIR
-// check for PAIR
-// if # of pairs === 2 & value[0] !== value[1], 2 PAIR EXISTS
+const checkForTwoPair = (hand) => {
+	// check for PAIR
+	// if # of pairs === 2 & value[0] !== value[1], 2 PAIR EXISTS
+}
 
 // PAIR
-// valueSort
-// valueCheck
-// if # of pairs === 1, PAIR EXISTS
+const checkForPair = (hand) => {
+	// valueSort
+	let resultingHand = {
+		...hand
+	};
+
+	let numPairs = 0;
+	let cards = [];
+
+	const updatePair = (currentHand, i) => {
+		currentHand.handTitle = 'Pair';
+		currentHand.handRank = 2;
+		currentHand.cardOrder = [resultingHand.cards[i], resultingHand.cards[i+1], resultingHand.cards[i+2], resultingHand.cards[i+3], resultingHand.cards[i+4]];
+		return currentHand;
+	};
+
+	// Sort by value; suit doesn't matter
+	if(resultingHand.sort !== 'valueSort') {
+		resultingHand = valueSort(resultingHand);
+	}
+
+	// Determine what pairs exist, and put the paired cards in the cards array
+	for (let i = 1; i < resultingHand.cards.length; i++) {
+		if (cards.length < 5 && resultingHand.cards[i].value === resultingHand.cards[i-1].value) {
+			numPairs++;
+			if (cards.indexOf(resultingHand.cards[i-1]) === -1) {
+				cards.push(resultingHand.cards[i-1]);
+			}
+			cards.push(resultingHand.cards[i]);
+		}
+	}
+
+	// Complete the cards array with the highest value of leftover cards
+	for (let i = 0; i < resultingHand.cards.length; i++) {
+		if (cards.length < 5 && cards.indexOf(resultingHand.cards[i]) === -1) {
+			cards.push(resultingHand.cards[i]);
+		}
+	}
+
+	// if # of pairs === 1, PAIR EXISTS
+	if (numPairs > 0) {
+		resultingHand = updatePair(resultingHand);
+		resultingHand.cardOrder = cards;
+		resultingHand.pairCards = cards;
+		resultingHand.numPairs = numPairs;
+	}
+
+	return resultingHand;
+}
 
 // HIGH CARD
+const checkForHighCard = (hand) => {
+	// valueSort
+	// update Card Order
+};
+
+console.log(checkForPair(initialHand));
